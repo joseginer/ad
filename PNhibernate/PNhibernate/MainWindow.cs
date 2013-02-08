a1a69953ceb3b8cd88a8cab3e0922747c9699198
Jose <jose@pcI5-18.(none)>
2013-02-08 09:36:18 +0100

    Actualizando Articulo

--- PNhibernate/PNhibernate/MainWindow.cs
+++ PNhibernate/PNhibernate/MainWindow.cs
@@ -36,8 +36,13 @@ public partial class MainWindow: Gtk.Window
 	private void loadArticulo(ISessionFactory sessionFactory) {
 		using (ISession session = sessionFactory.OpenSession()) {
 			Articulo articulo = (Articulo)session.Load (typeof(Articulo), 2L);
-			Console.WriteLine ("Articulo Id={0} Nombre={1} Precio={2}", 
+			Console.Write ("Articulo Id={0} Nombre={1} Precio={2}", 
 			                   articulo.Id, articulo.Nombre, articulo.Precio);
+			if(articulo.Categoria==null)
+			Console.WriteLine("Categoria=null");
+			else
+			Console.WriteLine("Categoria.Id={0}", articulo.Categoria.Id);
+			//Console.WriteLine("Categoria.Nombre={0}", articulo.Categoria.Id);
 		}
 	}
 	
